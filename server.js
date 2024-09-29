89f2ecb4f2051c7caa17f4ff8463b0d4439a6bfa
Doufi-7 <doufidoufi26@gmail.com>
2024-09-29 16:37:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,29 +4,31 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// استخدم cookie-parser لتحليل الكوكيز
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Configure session
+// إعداد الجلسة
 app.use(session({
-    secret: 'mySuperSecretKey12345678901234567890', // Change this to a strong secret key
+    secret: 'mySuperSecretKey12345678901234567890', // غيّر هذا إلى مفتاح سري قوي
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false } // Set to true if using HTTPS
+    cookie: { secure: false } // إذا كنت تستخدم HTTPS، غيّرها إلى true
 }));
 
+// معالجة المسار الجذر
 app.get('/', (req, res) => {
     res.send('Welcome to the Cookie Session Example!');
 });
 
-// Route to set a session variable
+// معالجة المسار لتعيين متغير الجلسة
 app.post('/set-session', (req, res) => {
-    req.session.user = req.body.user; // Set session variable
+    req.session.user = req.body.user; // تعيين متغير الجلسة
     res.send('Session variable set!');
 });
 
-// Route to get a session variable
+// معالجة المسار للحصول على متغير الجلسة
 app.get('/get-session', (req, res) => {
     if (req.session.user) {
         res.send(`Session variable: ${req.session.user}`);
@@ -35,6 +37,12 @@ app.get('/get-session', (req, res) => {
     }
 });
 
+// معالجة المسار /ea/all
+app.get('/ea/all', (req, res) => {
+    res.send('<h1>أهلاً وسهلاً بكم في صفحة /ea/all!</h1>');
+});
+
+// تشغيل السيرفر
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
